0a6dce75f4064f80ff21d03909e14db2abb6c42f
Hyuniverse <hyuniverse.dev@gmail.com>
2025-01-04 16:45:50 +0900

    oop

--- .venv/ch10/extend_practice1.py
+++ .venv/ch10/extend_practice1.py
@@ -18,6 +18,9 @@ class Person:
     def introduce(self):
         print(f"안녕하세요 제 이름은 {self.name}입니다. 저는 {self.age}살입니다.")
 
+    def hello(self):
+        print("hello world")
+
 
 # Student 클래스 정의 - Person 상속
 #   추가 속성: korean, math, english
@@ -28,6 +31,12 @@ class Student(Person):
         self.math = math
         self.english = english
 
+    def introduce(self):
+        print(f"안녕하세요 제 이름은 {self.name}입니다. 저는 {self.age}살 학생입니다.")
+
+    def student_method(self):
+        print("Student 클래스 메소드 입니다.")
+
 
 # Teacher 클래스 정의 - Person 상속
 #   추가 속성: subject
@@ -35,6 +44,9 @@ class Teacher(Person):
     def __init__(self, name, age, gender, subject):
         super().__init__(name, age, gender)
         self.subject = subject
+
+    def introduce(self):
+        print(f"안녕하세요 제 이름은 {self.name}입니다. 저는 {self.age}살 강사입니다.")
 
 
 ############################################
@@ -48,3 +60,11 @@ student.introduce()
 teacher = Teacher("박현", 20, "남성", "파이썬")
 print(teacher.name, teacher.age, teacher.gender, teacher.subject)
 teacher.introduce()
+
+############################################
+
+print(type(student) == Person)
+print(type(student) == Student)
+
+print(isinstance(student, Person))
+print(isinstance(student, Student))
