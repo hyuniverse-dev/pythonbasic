9fa8017af888d52e17199675611377df8a3f4731
Hyuniverse <hyuniverse.dev@gmail.com>
2025-01-04 15:24:18 +0900

    refactor file name

--- .venv/ch10/extend_ex1.py
+++ .venv/ch10/extend_ex1.py
@@ -15,9 +15,22 @@ class Parent:
         print("부모 클래스의 메소드가 호출되었습니다.")
 
 
-class Child:
+class Child(Parent):
     def __init__(self):
+        super().__init__()
         self.child_value = "Child"
 
     def child_method(self):
         print("자식 클래스의 메소드가 호출되었습니다.")
+
+
+# Child 클래스로 인스턴스 생성
+child = Child()
+
+# Child 클래스의 필드와 메소드 호출
+print(child.child_value)
+child.child_method()
+
+# Parent 클래스의 필드와 메소드 호출
+print(child.value)
+child.parent_method()
---
+++ .venv/ch10/extend_practice1.py
@@ -0,0 +1,20 @@
+'''
+파일명: extend_practice1.py
+위치: .venv/ch10/
+'''
+
+# Person 클래스 정의
+#   속성: name, age, gender
+#   기능: introduce()
+#         "안녕하세요. 제 이름은 000입니다. 저는 00살입니다."
+
+# Student 클래스 정의 - Person 상속
+#   추가 속성: korean, math, english
+
+# Teacher 클래스 정의 - Person 상속
+#   추가 속성: subject
+
+############################################
+
+# Student 인스턴스 생성 후 name, age, gender, korean, math, english 점수 출력
+# Teacher 인스턴스 생성 후 name, age, gender, subject 출력
