ac2717fc92e3b87b3d3df4b389a9d724ac5321ab
Hyuniverse <hyuniverse.dev@gmail.com>
2025-01-11 15:52:33 +0900

    filter words

--- .venv/ch12/file_ex6.py
+++ .venv/ch12/file_ex6.py
@@ -7,12 +7,43 @@ file_name = "황금별.txt"
 directory = "../data/"
 
 ### (1) 황금별.txt 파일 열기 (read모드)
-
+file = open(f"{directory}{file_name}", "rt", encoding='utf-8')
 
 ### (2) 파일로부터 불러들인 데이터를 읽어서 다음 단어가 몇 번 나오는지 찾기
 ###     - 지정 단어(1) 왕
 ###     - 지정 단어(2) 별
-
 ###     (2-1) readlines() 메소드로 텍스트 데이터 읽기
+lyrics = file.readlines()
+print(lyrics)
 
 ###     (2-2) for문으로 각 항목을 순회하면서 지정 단어 count 1씩 증가
+count_king = 0
+count_star = 0
+
+for word in lyrics:
+    for character in word:
+        if "왕" == character:
+            count_king += 1
+        if "별" == character:
+            count_star += 1
+
+print(f"왕: {count_king}번, 별: {count_star}번")
+file.close()
+
+### 버전(2)
+file = open(f"{directory}{file_name}", "rt", encoding="utf-8")
+
+lyrics = file.read()
+
+count_king = 0
+count_star = 0
+for word in lyrics:
+    if "왕" == word:
+        count_king += 1
+    if "별" == word:
+        count_star += 1
+
+print(f"왕: {count_king}번, 별: {count_star}번")
+file.close()
+
+
---
+++ .venv/ch12/file_ex7.py
@@ -0,0 +1,38 @@
+'''
+파일명: file_ex7.py
+위치: .venv/ch12/
+'''
+
+### with문
+file_name = "황금별.txt"
+directory = "../data/"
+
+with open(f"{directory}{file_name}", "rt", encoding="utf-8") as file:
+    lyrics = file.read()
+    print(lyrics)
+
+
+### with문으로 금칙어 필터링 프로그램 만들기
+###     파일에서 텍스트 데이터를 읽어와서 특정 금칙어를 "*"로 치환하는 프로그램을 작성하세요.
+###     치환할 때 기존 글자수만큼 "*"를 처리해주세요. (예시, 바보 -> **)
+###     금칙어(1): 놈
+###     금칙어(2): 바보
+###     별 처리된 결과를 콘솔에 출력
+
+filter_words = ["놈", "바보"]
+
+with open(f"{directory}댓글.txt", "rt", encoding='utf-8') as file:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
